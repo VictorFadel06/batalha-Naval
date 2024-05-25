816478dab20def079a4a422291b6f01d49c804ca
VictorFadel06 <victorfadel96@gmail.com>
2024-05-24 21:38:50 -0300

    ajustado sistema de acertos, diferenciando embarcações acertadas

--- batalha_naval.py
+++ batalha_naval.py
@@ -3,6 +3,9 @@ from random import randint
 total_barcos_jogador = 5
 total_barcos_computador = 5
 
+print("SUPER BLASTER BATALHA NAVAL")
+print('-'*50)
+
 def matriz_jogador_1():
   matriz_1 = []
 
@@ -48,9 +51,14 @@ def atirar(matriz, turn):
     coluna = randint(0,9)
   if matriz[linha][coluna] == 'B':
     print("Você acertou a embarcação!")
+    matriz[linha][coluna] = 'X'
     pontuation(turn)
+  elif matriz[linha][coluna] == 'X':
+    print("Embarcação JÁ destruída!")
   else:
     print("Você ERROU!")
+    matriz[linha][coluna] = 'N'
+    
 
 def player_turn():
   starts = 'player_1'
@@ -69,16 +77,39 @@ def pontuation(turn):
     total_barcos_computador -= 1
   elif turn == 'player_2':
     total_barcos_jogador -= 1
-  print(f'barcos do jogador: {total_barcos_jogador}')
-  print(f'barcos do computador: {total_barcos_computador}')
+  # print(f'barcos do jogador: {total_barcos_jogador}')
+  # print(f'barcos do computador: {total_barcos_computador}')
+  
 
 matriz_1 = matriz_jogador_1()
 matriz_2 = matriz_jogador_2()
 
-print_matriz(matriz_1)
+
 print('\n')
+print('Tabuleiro do Jogador')
+print_matriz(matriz_1)
+print('-'*50)
+print(f'Embarcações restantes: {total_barcos_jogador}')
+print('\n')
+print('Tabuleiro do Computador')
 print_matriz(matriz_2)
+print('-'*50)
+print(f'Embarcações restantes: {total_barcos_computador}')
 print('\n')
 
 while total_barcos_computador != 0 and total_barcos_jogador != 0:
   player_turn()
+  print('Tabuleiro do Jogador')
+  print_matriz(matriz_1)
+  print('-'*50)
+  print(f'Embarcações restantes: {total_barcos_jogador}')
+  print('\n')
+  print('Tabuleiro do Computador')
+  print_matriz(matriz_2)
+  print('-'*50)
+  print(f'Embarcações restantes: {total_barcos_computador}')
+  print('\n')
+  if total_barcos_jogador == 0:
+    print("COMPUTADOR Ganhou!!!")
+  if total_barcos_computador == 0:
+    print("JOGADOR Ganhou!!!")
