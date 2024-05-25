cded6e46cc6a849d0aa76456cd20f4702d0fb183
VictorFadel06 <victorfadel96@gmail.com>
2024-05-24 22:52:45 -0300

    criado tabuleiros separados do tabuleiro principal

--- batalha_naval.py
+++ batalha_naval.py
@@ -1,4 +1,5 @@
 from random import randint
+import os
 
 total_barcos_jogador = 5
 total_barcos_computador = 5
@@ -38,11 +39,31 @@ def matriz_jogador_2():
 
   return matriz_2
 
+def desenhar_matriz_1():
+  matriz_desenhada1 = []
+
+  for i in range(10):
+    matriz_desenhada1.append([])
+    for j in range(10):
+      matriz_desenhada1[i].append('-')
+
+  return matriz_desenhada1
+
+def desenhar_matriz_2():
+  matriz_desenhada2 = []
+
+  for i in range(10):
+    matriz_desenhada2.append([])
+    for j in range(10):
+      matriz_desenhada2[i].append('-')
+
+  return matriz_desenhada2
+
 def print_matriz(matriz):
   for linha in matriz:
     print(linha)
 
-def atirar(matriz, turn):
+def atirar(matriz, matriz_desenhada, turn):
   if turn == 'player_1':
     linha = int(input("Informe a linha do adversário: "))
     coluna = int(input("Informe a coluna do adversário: "))
@@ -52,23 +73,26 @@ def atirar(matriz, turn):
   if matriz[linha][coluna] == 'B':
     print("Você acertou a embarcação!")
     matriz[linha][coluna] = 'X'
+    matriz_desenhada[linha][coluna] = 'X'
     pontuation(turn)
   elif matriz[linha][coluna] == 'X':
     print("Embarcação JÁ destruída!")
   else:
     print("Você ERROU!")
-    matriz[linha][coluna] = 'N'
+    matriz_desenhada[linha][coluna] = 'N'
     
 
 def player_turn():
   starts = 'player_1'
   if starts == 'player_1':
-      print('JOGADOR: ')
-      atirar(matriz_2, starts)
-      starts = 'player_2'
+    # os.system('cls')
+    print('JOGADOR: ')
+    atirar(matriz_2, matriz_desenhada2, starts)
+    starts = 'player_2'
   if starts == 'player_2':
+    # os.system('cls')
     print('COMPUTADOR: ')
-    atirar(matriz_1, starts)
+    atirar(matriz_1, matriz_desenhada1, starts)
 
 def pontuation(turn):
   global total_barcos_computador
@@ -77,22 +101,22 @@ def pontuation(turn):
     total_barcos_computador -= 1
   elif turn == 'player_2':
     total_barcos_jogador -= 1
-  # print(f'barcos do jogador: {total_barcos_jogador}')
-  # print(f'barcos do computador: {total_barcos_computador}')
   
 
 matriz_1 = matriz_jogador_1()
 matriz_2 = matriz_jogador_2()
+matriz_desenhada1 = desenhar_matriz_1()
+matriz_desenhada2 = desenhar_matriz_2()
 
 
 print('\n')
 print('Tabuleiro do Jogador')
-print_matriz(matriz_1)
+print_matriz(matriz_desenhada1)
 print('-'*50)
 print(f'Embarcações restantes: {total_barcos_jogador}')
 print('\n')
 print('Tabuleiro do Computador')
-print_matriz(matriz_2)
+print_matriz(matriz_desenhada2)
 print('-'*50)
 print(f'Embarcações restantes: {total_barcos_computador}')
 print('\n')
@@ -100,12 +124,12 @@ print('\n')
 while total_barcos_computador != 0 and total_barcos_jogador != 0:
   player_turn()
   print('Tabuleiro do Jogador')
-  print_matriz(matriz_1)
+  print_matriz(matriz_desenhada1)
   print('-'*50)
   print(f'Embarcações restantes: {total_barcos_jogador}')
   print('\n')
   print('Tabuleiro do Computador')
-  print_matriz(matriz_2)
+  print_matriz(matriz_desenhada2)
   print('-'*50)
   print(f'Embarcações restantes: {total_barcos_computador}')
   print('\n')
